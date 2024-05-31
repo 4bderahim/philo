4e8d8acc1dcef4134034a07fff8ab85632655705
4bderahim <abderahimelkridah@gmail.com>
2024-06-01 00:29:52 +0100

    sd

--- main.c
+++ main.c
@@ -10,7 +10,8 @@ void *thread(void* arg)
     long int i;
     struct s_tinfo pth;
 
-    pth.mutex = (struct s_tinfo *) arg;
+    pth.mutex = *(pthread_mutex_t *)arg;
+    
     i = 0;
     while (i < 10000000000)
     {
@@ -28,13 +29,13 @@ int main()
     pthread_mutex_t mutexx;
     struct s_tinfo thread_struct;
     thread_struct.mutex = mutexx;
-    pthread_mutex_ini(&mutexx, NULL);
+    pthread_mutex_init(&mutexx, NULL);
     int t;
     t = 0;
-    pthread_create(&threads, NULL, thread, &thread_struct);
+    pthread_create(&threads, NULL, thread, &thread_struct.mutex);
     pthread_join(threads, NULL);
-    usleep(111000000);
-    pthread_create(&threads1, NULL, thread, &thread_struct);
+    //usleep(111000000);
+    pthread_create(&threads1, NULL, thread, &thread_struct.mutex);
     pthread_join(threads1, NULL);
     //     t++;
     // }
