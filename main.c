8d04a1e2ffbc98419a38a47806d094f129e3c031
4bderahim <abderahimelkridah@gmail.com>
2024-06-01 00:41:38 +0100

    NeeoOooOoo

--- main.c
+++ main.c
@@ -2,40 +2,41 @@
 #include "philo.h"
 
 
-
+pthread_mutex_t mutexx;
 int x = 0;
-
 void *thread(void* arg)
 {
     long int i;
-    struct s_tinfo pth;
+    //struct s_tinfo pth;
 
-    pth.mutex = *(pthread_mutex_t *)arg;
+    //pth.mutex = *(pthread_mutex_t *)arg;
     
     i = 0;
     while (i < 10000000000)
     {
-        pthread_mutex_lock(&pth.mutex);
+        pthread_mutex_lock(&mutexx);
         i++;
         x++;
-        pthread_mutex_unlock(&pth.mutex);
+        pthread_mutex_unlock(&mutexx);
     }
+
     return (arg);
 }
 int main()
 {
     pthread_t threads;
     pthread_t threads1;
-    pthread_mutex_t mutexx;
+    
     struct s_tinfo thread_struct;
-    thread_struct.mutex = mutexx;
+   // thread_struct.mutex = mutexx;
     pthread_mutex_init(&mutexx, NULL);
     int t;
     t = 0;
-    pthread_create(&threads, NULL, thread, &thread_struct.mutex);
+    pthread_create(&threads, NULL, thread, "");
+    pthread_create(&threads1, NULL, thread, "");
     pthread_join(threads, NULL);
     //usleep(111000000);
-    pthread_create(&threads1, NULL, thread, &thread_struct.mutex);
+    
     pthread_join(threads1, NULL);
     //     t++;
     // }
