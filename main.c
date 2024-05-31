ca11af37a3a62ea59e11bd001d181b1b2007316d
4bderahim <abderahimelkridah@gmail.com>
2024-06-01 00:20:47 +0100

    emjoying Mutex locking ..

--- main.c
+++ main.c
@@ -8,13 +8,16 @@ int x = 0;
 void *thread(void* arg)
 {
     long int i;
+    struct s_tinfo pth;
 
+    pth.mutex = (struct s_tinfo *) arg;
     i = 0;
     while (i < 10000000000)
     {
-        //pthread_mutex_lock()
+        pthread_mutex_lock(&pth.mutex);
         i++;
         x++;
+        pthread_mutex_unlock(&pth.mutex);
     }
     return (arg);
 }
@@ -22,26 +25,20 @@ int main()
 {
     pthread_t threads;
     pthread_t threads1;
-    pthread_mutex_t mutex;
-    //struct s_tinfo thread_struct;
-
-    //thread_struct.mutex = mutex;
-
-    
-
-
-    pthread_mutex_ini(&mutex, NULL);
-
+    pthread_mutex_t mutexx;
+    struct s_tinfo thread_struct;
+    thread_struct.mutex = mutexx;
+    pthread_mutex_ini(&mutexx, NULL);
     int t;
     t = 0;
-    pthread_create(&threads, NULL, thread, "new thread");
+    pthread_create(&threads, NULL, thread, &thread_struct);
     pthread_join(threads, NULL);
     usleep(111000000);
-    pthread_create(&threads1, NULL, thread, "new thread");
+    pthread_create(&threads1, NULL, thread, &thread_struct);
     pthread_join(threads1, NULL);
     //     t++;
     // }
     printf(">>%d\n", x);
-    //pthread_mutex_destroy(mutex);
+    pthread_mutex_destroy(&mutexx);
     return (0);
 }
