548e77083c0a95f393fb2d0b731e3c47cdc22c20
4bderahim <abderahimelkridah@gmail.com>
2024-06-02 00:33:40 +0100

    mutexer

--- main.c
+++ main.c
@@ -11,13 +11,14 @@ void *thread(void* arg)
 
     i = 0;
     pthread_mutex_lock(&mutexx->mutex);
-    printf("[+] thread %d is counting\n", mutexx->p_id);
-    while (i < 1000000000)
+    while (i < 100000000)
     {
         i++;
         x++;
     }
+    
     pthread_mutex_unlock(&mutexx->mutex);    
+   
     return (0);
 }
 int main()
@@ -26,27 +27,40 @@ int main()
     pthread_mutex_t mutexx;
     struct s_tinfo thread_struct;
     thread_struct.mutex = mutexx;
+    pthread_mutex_init(&thread_struct.mutex, NULL);
+    
     int i;
     i = 0;
     while (i < 5)
     {
-        pthread_create(&threads[i], NULL, thread, &thread_struct);
+        if (pthread_create(&threads[i], NULL, thread, &thread_struct) !=0)
+            {
+                printf("neitttt!!");
+                return (0);
+            }
         printf("[!] thread %d is alive\n", i);
         thread_struct.p_id = i;
+         
+        i++;
+    }
+    i = 0;
+    while (i < 5)
+    {
+        //thread_struct.p_id = i;
         pthread_join(threads[i], NULL);
+         printf("[+] thread %d is done\n", i);
         i++;
     }
     
-    
     // thread_struct.mutex = mutexx;
-    // pthread_mutex_init(&thread_struct.mutex, NULL);
+    
 
     // pthread_create(&threads, NULL, thread, &thread_struct);
     // pthread_create(&threads1, NULL, thread, &thread_struct);
     // pthread_join(threads, NULL);
     // pthread_join(threads1, NULL);
     
-    // printf(">>%ld\n", x);
+     printf(">>%ld\n", x);
     // pthread_mutex_destroy(&mutexx);
     return (0);
 }
