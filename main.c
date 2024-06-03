6efab814eca5fde300d27cfbe669e0396d29a349
4bderahim <abderahimelkridah@gmail.com>
2024-06-03 12:32:14 +0100

    struct logiv

--- main.c
+++ main.c
@@ -4,9 +4,9 @@
 long int x = 0;
 void *thread(void* arg)
 {
+    
     long int i;
     struct s_tinfo *mutexx;
-
     mutexx = (struct s_tinfo *)arg;
 
     i = 0;
@@ -16,51 +16,73 @@ void *thread(void* arg)
         i++;
         x++;
     }
-    
     pthread_mutex_unlock(&mutexx->mutex);    
    
     return (0);
 }
-int main()
+void get()
 {
-    pthread_t threads[5];
-    pthread_mutex_t mutexx;
-    struct s_tinfo thread_struct;
-    thread_struct.mutex = mutexx;
-    pthread_mutex_init(&thread_struct.mutex, NULL);
-    
+    t_test *hello;
     int i;
     i = 0;
+    hello = (t_test *)malloc(sizeof(t_test *) * 5);
     while (i < 5)
     {
-        if (pthread_create(&threads[i], NULL, thread, &thread_struct) !=0)
-            {
-                printf("neitttt!!");
-                return (0);
-            }
-        printf("[!] thread %d is alive\n", i);
-        thread_struct.p_id = i;
-         
+        (hello+i)->x = i *2;
+        (hello+i)->filo_num = i*i;
         i++;
     }
     i = 0;
     while (i < 5)
     {
-        //thread_struct.p_id = i;
-        pthread_join(threads[i], NULL);
-         printf("[+] thread %d is done\n", i);
+        printf("\t%d|%d\n",        (hello+i)->x, (hello+i)->filo_num );
         i++;
     }
+}
+int main()
+{
+    int i;
+    i = 0;
+    get();
+    system("leaks -q a.out");
+}
+
+// int main()
+// {
+//     pthread_t threads[5];
+//     pthread_mutex_t mutexx;
+//     struct s_tinfo thread_struct;
+//     thread_struct.mutex = mutexx;
+//     pthread_mutex_init(&thread_struct.mutex, NULL);
     
-    // thread_struct.mutex = mutexx;
+//     int i;
+//     i = 0;
+//     while (i < 5)
+//     {
+//         pthread_create(&threads[i], NULL, thread, &thread_struct);
+//         printf("[!] thread %d is alive\n", i);
+//         thread_struct.p_id = i;
+         
+//         i++;
+//     }
+//     i = 0;
+//     while (i < 5)
+//     {
+//         //thread_struct.p_id = i;
+//         pthread_join(threads[i], NULL);
+//          printf("[+] thread %d is done\n", i);
+//         i++;
+//     }
+    
+//     // thread_struct.mutex = mutexx;
     
 
-    // pthread_create(&threads, NULL, thread, &thread_struct);
-    // pthread_create(&threads1, NULL, thread, &thread_struct);
-    // pthread_join(threads, NULL);
-    // pthread_join(threads1, NULL);
+//     // pthread_create(&threads, NULL, thread, &thread_struct);
+//     // pthread_create(&threads1, NULL, thread, &thread_struct);
+//     // pthread_join(threads, NULL);
+//     // pthread_join(threads1, NULL);
     
-     printf(">>%ld\n", x);
-    // pthread_mutex_destroy(&mutexx);
-    return (0);
-}
+//      printf(">>%ld\n", x);
+//     // pthread_mutex_destroy(&mutexx);
+//     return (0);
+// }
--- philo.h
+++ philo.h
@@ -3,10 +3,16 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
-
+#include <stdlib.h>
 struct s_tinfo
 {
     pthread_mutex_t mutex;
-    int p_id;
-    
+    int p_id;  
 };
+
+typedef struct s_test
+{
+    int filo_num;
+    int x;  
+} t_test;
+
